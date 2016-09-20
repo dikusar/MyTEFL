93984a766901ddd0444f49c47eb40e227d9e17ca
AlfimovDev <kalfimov@gmail.com>
2016-09-20 23:13:21 +0300

    100% FAQ page completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,26 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+
+init_accordion ();
+// Accordion
+function init_accordion () {
+    $(".accordion").each(function() {
+        var allPanels = $(this).children('.content').hide();
+        allPanels.first().slideDown("easeOutExpo");
+        $(this).children('.content-title').first().addClass("active");
+
+        $(this).children('.content-title').on('click', function(){
+
+            var current = $(this).next(".content");
+            $(this).parent().children('.content-title').removeClass("active");
+            $(this).addClass("active");
+            allPanels.not(current).slideUp("easeInExpo");
+            $(this).next().slideDown("easeOutExpo");
+            
+            return false;
+            
+        });
+    })
+    
+}
