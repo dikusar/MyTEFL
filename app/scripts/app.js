cb591c68f0d9191d6a924460bc828adb86c51eaa
dikusar <lovipochtuarnold@gmail.com>
2016-10-22 02:05:43 +0300

    js(nav-bar mixin): set js handler to show/hide mobile menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,25 +5,43 @@ $(() => {
 	svg4everybody();
 });
 
+function mobileMenuHandler() {
+	const mobIcon = '.js-nav-bar-menu-mobile-icon';
+	const mobMenu = '.js-nav-bar-menu-list-mobile';
+	const mobMenuDislayClass = 'display';
+
+	$(mobIcon).on('click tap', ()=> {
+		$(mobMenu).toggleClass(mobMenuDislayClass);
+	});
+
+	$(document).on("click tap", e=> {
+		if (!$(mobIcon).is(e.target)  && !$(mobMenu).is(e.target) && $(mobMenu).has(e.target).length === 0) {
+			$(mobMenu).removeClass(mobMenuDislayClass);
+		}
+	});
+}
+
+mobileMenuHandler();
+ 
 // Accordion
-function init_accordion() {
-    $(".accordion").each( function() {
-        var allPanels = $(this).children('.content').hide();
-        allPanels.first().slideDown("easeOutExpo");
-        $(this).children('.content-title').first().addClass("active");
+function initAccordion() {
+	$(".accordion").each( function() {
+		var allPanels = $(this).children('.content').hide();
+		allPanels.first().slideDown("easeOutExpo");
+		$(this).children('.content-title').first().addClass("active");
 
-        $(this).children('.content-title').on('click', function() {
+		$(this).children('.content-title').on('click', function() {
 
-            var current = $(this).next(".content");
-            $(this).parent().children('.content-title').removeClass("active");
-            $(this).addClass("active");
-            allPanels.not(current).slideUp("easeInExpo");
-            $(this).next().slideDown("easeOutExpo");
-            
-            return false;
-            
-        });
-    })
+			var current = $(this).next(".content");
+			$(this).parent().children('.content-title').removeClass("active");
+			$(this).addClass("active");
+			allPanels.not(current).slideUp("easeInExpo");
+			$(this).next().slideDown("easeOutExpo");
+			
+			return false;
+			
+		});
+	})
    
 }
-init_accordion ();
+initAccordion();
